fd2d5b82e962d28f514b4cfb9b6f5d44a9c65aba
w <pcranaway@tuta.io>
2022-08-15 22:22:03 +0300

    add cssls

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -2,7 +2,7 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "sumneko_lua", "rust_analyzer", "sqls", "tsserver", "astro" }
+local servers = { "sumneko_lua", "rust_analyzer", "sqls", "tsserver", "astro", "cssls" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
